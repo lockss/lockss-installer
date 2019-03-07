7d980a6c3c53e3da965ba704fd9e478cc4ffdb89
Claire Griffin <clairetg@stanford.edu>
2019-03-07 02:11:21 -0800

    cleanup of config files

--- config/conf/conf.sh
+++ config/conf/conf.sh
@@ -27,23 +27,22 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-. ../config.dat
 
 MVN_GRP='org.lockss.laaws'
 
-UI_USER=$(LOCKSS_ADMIN_USER)
-UI_PASS=$(LOCKSS_ADMIN_PASSWD)
+UI_USER=claire
+UI_PASS=SHA-256:b515ed947183189479f29b35d92b58f431e6ac3a787471c30299686869d774cf
 CFG_GRP="${MVN_GRP}"
 CFG_ART='laaws-configuration-service'
 CFG_VERSION='1.1.0-SNAPSHOT'
-CFG_HOST=$(LOCKSS_CFG_HOST)
+CFG_HOST=lockss-configuration-service
 CFG_REST_PORT='24620'
 CFG_UI_PORT='24621'
 CFG_CMD="-b config/cluster/bootstrap.txt
          -l config/cluster/cluster.txt
          -l config/cluster/cluster.opt
-         -p config/lockss-configuration-service/lockss.txt
-         -p config/lockss-configuration-service/lockss.opt"
+         -p config/services/lockss-configuration-service/lockss.txt
+         -p config/services/lockss-configuration-service/lockss.opt"
 CFG_URL="http://${UI_USER}:${UI_PASS}@${CFG_HOST}:${CFG_REST_PORT}"
 
 JMS_HOST="${CFG_HOST}"
@@ -52,14 +51,14 @@ JMS_PORT='61616'
 MDQ_GRP="${MVN_GRP}"
 MDQ_ART='laaws-metadata-service'
 MDQ_VERSION='1.0.0-SNAPSHOT'
-MDQ_HOST=$(LOCKSS_MDQ_HOST)
+MDQ_HOST=lockss-metadata-service
 MDQ_REST_PORT='24650'
 MDQ_UI_PORT='24651'
 MDQ_CMD="-b config/cluster/bootstrap.txt
          -c ${CFG_URL}
          -p ${CFG_URL}/config/file/cluster
-         -p config/lockss-metadata-service/lockss.txt
-         -p config/lockss-metadata-service/lockss.opt"
+         -p config/services/lockss-metadata-service/lockss.txt
+         -p config/services/lockss-metadata-service/lockss.opt"
 
 MDX_GRP="${MVN_GRP}"
 MDX_ART='laaws-metadata-extraction-service'
@@ -69,8 +68,8 @@ MDX_UI_PORT='24641'
 MDX_CMD="-b config/cluster/bootstrap.txt
          -c ${CFG_URL}
          -p ${CFG_URL}/config/file/cluster
-         -p config/lockss-metadata-extraction-service/lockss.txt
-         -p config/lockss-metadata-extraction-service/lockss.opt"
+         -p config/services/lockss-metadata-extraction-service/lockss.txt
+         -p config/services/lockss-metadata-extraction-service/lockss.opt"
 
 POL_GRP="${MVN_GRP}"
 POL_ART='laaws-poller'
@@ -80,35 +79,34 @@ POL_UI_PORT='24631'
 POL_CMD="-b config/cluster/bootstrap.txt
          -c ${CFG_URL}
          -p ${CFG_URL}/config/file/cluster
-         -p config/lockss-poller/lockss.txt
-         -p config/lockss-poller/lockss.opt"
+         -p config/services/lockss-poller/lockss.txt
+         -p config/services/lockss-poller/lockss.opt"
 
 # LAAWS repository service configuration
 REPO_GRP="${MVN_GRP}"
 REPO_ART='laaws-repository-service'
 REPO_VERSION='1.8.0-SNAPSHOT'
 REPO_REST_PORT='24610'
-REPO_CMD="--spring.config.location=file:./config/lockss-repository-service/repo.properties,file:./config/lockss-repository-service/repo.properties.opt,file"
-REPO_JARGS="-Dorg.lockss.jmsUri=tcp://${LOCKSS_JMS_HOST}:${LOCKSS_JMS_PORT}"
+REPO_CMD="--spring.config.location=file:./config/services/lockss-repository-service/repo.properties,file:./config/services/lockss-repository-service/repo.properties.opt"
+REPO_JARGS="-Dorg.lockss.jmsUri=tcp://lockss-configuration-service:61616"
 REPO_BASEDIR=/lockss # TODO: Propagate this to projects
 REPO_MAX_WARC_SIZE=1048576 # 1 MB
 
 # PostgreSQL database configuration
 PGSQL_VERSION='9.6'
-PGSQL_HOST=$(LOCKSS_PGSQL_HOST)
+PGSQL_HOSTlockss-metadata-pgsql
 PGSQL_PORT='5432'
-#todo should these be the admin user/password?
 POSTGRES_USER=LOCKSS
 POSTGRES_PASSWORD=goodPassword
 POSTGRES_DB=postgres
 
 # Solr container configuration
-SOLR_HOST=$(LOCKSS_SOLR_HOST)
+SOLR_HOST=lockss-solr
 SOLR_PORT='8983'
 SOLR_CMD='solr-precreate-several.sh demo test-core'
 
 # HDFS container configuration
-HDFS_HOST=$(LOCKSS_HDFS_HOST)
+HDFS_HOST=lockss-hdfs
 HDFS_FSMD='9000'
 HDFS_DATA='50010'
 HDFS_MD='50020'
@@ -124,7 +122,7 @@ PYWB_HDFSMNT=/mnt/hdfs
 PYWB_WATCHDIR=${PYWB_HDFSMNT}/${REPO_BASEDIR}/sealed
 
 # EDINA indexer settings
-LOCKSS_SOLR_HDFSMNT=/laaws-demo-hdfs
+LOCKSS_SOLR_HDFSMNT=/lockss-hdfs
 LOCKSS_SOLR_WATCHDIR=${LOCKSS_SOLR_HDFSMNT}/${REPO_BASEDIR}/sealed
 LOCKSS_SOLR_WATCHDIR_INTERVAL=10 # Seconds
 LOCKSS_SOLR_URL=http://${SOLR_HOST}:${SOLR_PORT}/solr/test-core
