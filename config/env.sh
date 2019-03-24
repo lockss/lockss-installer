7034592d99f89c7e10ec38bceb2820caed53183f
Thib Guicherd-Callin <thib@cs.stanford.edu>
2019-03-23 17:59:02 -0700

    Squashed commit of the following:
    commit bcfd7e8bea6bbb624bb9db6a790b5c56eb952c64
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Sat Mar 23 15:34:04 2019 -0700
    
        Fix syntax
    
    commit 0c8922c95b96282f19fe12a3cad924de0c42f57c
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Sat Mar 23 15:33:07 2019 -0700
    
        Fix syntax
    
    commit 891b130e099d5150a18541bfc317c3f70764a18e
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Sat Mar 23 15:23:12 2019 -0700
    
        Typo.
    
    commit 89cc997fd8bc6ec82ca7596f289098b0becde7e8
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Sat Mar 23 02:27:19 2019 -0700
    
        added secrets, config and volumes
    
    commit c44c26dcafe17723319ac1dba6224edb2fa46c6d
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Fri Mar 22 17:39:59 2019 -0700
    
        fix circular dependency
    
    commit e700635ee0ebec536386def517e6ee5712b25a6c
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Fri Mar 22 03:10:16 2019 -0700
    
        Repo-related modifications.
    
    commit 9f4e77af939363afd9fac2a896f1ebadbfb87add
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Thu Mar 21 11:29:02 2019 -0700
    
        Add comment
    
    commit e3ebfac6a27ba9933907964cdcf516290982df37
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Thu Mar 21 03:20:51 2019 -0700
    
        Added the necessary lockss_service for the configuration service.
        Renamed lockss_cluster to match final name.
    
    commit 2e16743fe7c304a3f612cfef2fa859b7d50959fb
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Wed Mar 20 21:34:54 2019 -0700
    
        merged changes
    
    commit e1c622c47f3e9220393d7f01421f509ae7495874
    Merge: b8ea38b d788d58
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Wed Mar 20 20:53:57 2019 -0700
    
        Merge remote-tracking branch 'origin/feature-cfgsec2' into feature-docker_cfgsec
    
        # Conflicts:
        #	config/templates/bootstrap.erb
    
    commit d788d584735c32839f590552babceb7c00945899
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Wed Mar 20 19:15:54 2019 -0700
    
        Turned several service configs into installer templates.
        Expanded MDX, MDQ database values.
        Fixed errors.
        Cosmetic.
    
    commit b8ea38b80d9f024ee0a53259871dfd572a6b1220
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Wed Mar 20 18:33:23 2019 -0700
    
        fix tiller globals
    
    commit 984bd750912af4d522b762880f6ecdefbcdc722a
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Wed Mar 20 18:28:34 2019 -0700
    
        readd tiller support
    
    commit 26053cc1e9534934092edeb8fa50ffad6c59b5b4
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Wed Mar 20 15:51:51 2019 -0700
    
        param changes
    
    commit bb98e6771e6a4752438f81df48b1b07d11000dde
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Tue Mar 19 18:37:05 2019 -0700
    
        snapshot of configs
    
    commit edc990420f1de8da51b5dafb18c76d5c3b916b44
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Tue Mar 19 11:52:50 2019 -0700
    
        review changes
    
    commit 24331b6d0a88cf6bd29e12ce5482bfd4b069b28b
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Mon Mar 18 16:45:51 2019 -0700
    
        mostly cleaned up compose file
    
    commit b13d73335032eb91a6ed7b33a5bddf67bbe6a682
    Author: Thib Guicherd-Callin <thib@cs.stanford.edu>
    Date:   Sun Mar 17 15:30:16 2019 -0700
    
        Remove text indexer. Headers for legibility.
    
    commit d56123019ca01fe297883d052c468510f668d3a9
    Author: Claire Griffin <clairetg@stanford.edu>
    Date:   Fri Mar 15 18:10:59 2019 -0700
    
        snapshot of installer changes

---
+++ config/env.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+# Copyright (c) 2000-2018, Board of Trustees of Leland Stanford Jr. University
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##
+## Load user conf
+##
+
+
+CFG_IMG='lockss/laaws-configuration-service'
+CFG_VERSION='1.1.0-SNAPSHOT'
+CFG_HOST='lockss-configuration-service'
+CFG_REST_PORT='24620'
+CFG_UI_PORT='24621'
+CFG_URL="http://${LOCKSS_ADMIN_USER}:${LOCKSS_ADMIN_PASSWORD}@${CFG_HOST}:${CFG_REST_PORT}"
+
+JMS_HOST="${CFG_HOST}"
+JMS_DEFAULT_PORT='61616'
+JMS_PORT='24606'
+JMS_URI="tcp://${JMS_HOST}:${JMS_PORT}"
+
+MDQ_IMG='lockss/laaws-metadata-service'
+MDQ_VERSION='1.0.0-SNAPSHOT'
+MDQ_HOST=lockss-metadata-service
+MDQ_REST_PORT='24650'
+MDQ_UI_PORT='24651'
+
+MDX_IMG='lockss/laaws-metadata-extraction-service'
+MDX_VERSION='1.1.0-SNAPSHOT'
+MDX_REST_PORT='24640'
+MDX_UI_PORT='24641'
+
+POL_IMG='lockss/laaws-poller'
+POL_VERSION='1.0.0-SNAPSHOT'
+POL_REST_PORT='24630'
+POL_UI_PORT='24631'
+
+# LAAWS repository service configuration
+REPO_IMG='lockss/laaws-repository-service'
+REPO_VERSION='1.8.0-SNAPSHOT'
+REPO_HOST=lockss-repository-service
+REPO_REST_PORT='24610'
+REPO_BASEDIR='/lockss'
+
+# PostgreSQL database configuration
+POSTGRES_IMG='postgres'
+POSTGRES_VERSION='9.6.12-alpine'
+POSTGRES_HOST='lockss-postgres'
+POSTGRES_DEFAULT_PORT='5432'
+POSTGRES_PORT='24602'
+POSTGRES_USER=LOCKSS
+POSTGRES_DB=postgres
+
+# Solr container configuration
+LOCKSS_SOLR_IMG='solr'
+SOLR_VERSION='6.6.0-alpine'
+SOLR_HOST='lockss-solr'
+SOLR_DEFAULT_PORT='8983'
+SOLR_PORT='24603'
+SOLR_CORE_REPO='lockss-repo'
+SOLR_CMD="solr-precreate ${SOLR_CORE_REPO}"
+
+# HDFS container configuration
+HDFS_IMG='lockss/lockss-hdfs'
+HDFS_VERSION='1:1'
+HDFS_HOST=lockss-hdfs
+HDFS_FS_PORT='24619'
+HDFS_DEFAULT_FS_PORT='9000'
+HDFS_STATUI_PORT='50070'
+HDFS_DNUI_PORT='50075'
+
+# PyWb container settings
+PYWB_IMG='lockss/lockss-pywb'
+PYWB_VERSION='2.2.20190311-1'
+PYWB_HOST='lockss-repository-service'
+PYWB_PORT='24681'
+PYWB_DEFAULT_PORT='8080'
