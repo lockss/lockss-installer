d57adf0ed4278095bb5098607890d56196098e1a
Claire Griffin <clairetg@stanford.edu>
2019-09-05 04:32:32 -0700

    add support for db update

---
+++ scripts/alpha-to-beta-db-rename-after.sql
@@ -0,0 +1,6 @@
+DROP DATABASE "LockssMetadataDbManager";
+ALTER DATABASE "MetadataDbManager" RENAME TO "LockssMetadataDbManager";
+DROP DATABASE "LockssConfigDbManager";
+ALTER DATABASE "ConfigDbManager" RENAME TO "LockssConfigDbManager";
+DROP DATABASE "LockssJobDbManager";
+ALTER DATABASE "JobDbManager" RENAME TO "LockssJobDbManager";
