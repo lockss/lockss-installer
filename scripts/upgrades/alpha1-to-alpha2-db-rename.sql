0029919ee52457b10c0e58e6bb1034d5c84731f2
Thib Guicherd-Callin <thib@cs.stanford.edu>
2021-05-07 11:27:20 -0700

    Remove alpha1 to alpha2 upgrade artifacts.

--- scripts/upgrades/alpha1-to-alpha2-db-rename.sql
+++
@@ -1,3 +0,0 @@
-ALTER DATABASE "MetadataDbManager" RENAME TO "LockssMetadataDbManager";
-ALTER DATABASE "ConfigDbManager" RENAME TO "LockssConfigDbManager";
-ALTER DATABASE "JobDbManager" RENAME TO "LockssJobDbManager";
